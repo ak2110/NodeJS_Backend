abedc9cef0eda360e979b2167f3e6da755a86689
ak2110 <anaghakadoo@gmail.com>
2021-09-25 17:07:31 +0530

    Node callbacks and error handling| Added comments

--- node-test/rectangle.js
+++ node-test/rectangle.js
@@ -1,3 +1,8 @@
+/**
+* A simple node module demonstration.
+* Rectangle module is used in test_rectangle and is imported as a module
+*
+*/
 module.exports = (x, y, callback) => {
     if (x <= 0 || y <= 0) {
         setTimeout(() => callback(new Error("Rectangle is not valid for the given length l = " +
